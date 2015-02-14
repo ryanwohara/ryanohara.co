ec206d8283d8bfba273e4efb21ef5963807efd7f
Ryan W O'Hara <ryan@ryanwohara.com>
2015-02-14 02:08:23 -0500

    Code is what you make it.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,17 @@
 <html>
 	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Tangerine:100' rel='stylesheet' type='text/css'>
 		<link href='/css/app.css' rel='stylesheet' type='text/css'>
 
 		<script src="/js/script.js" type="text/javascript"></script>
 	</head>
 	<body>
-		<div class="container-fluid">
+		<div class="container">
 			<div class="content">
 				<div class="row">
 					<div class="title">Ryon</div>
-					<div class="quote">Code code code no matter what </div>
+					<div class="quote">Code is what you make it.</div>
+					<span class="glyphicon-home" aria-hidden="true"></span>
 				</div>
 			</div>
 		</div>
