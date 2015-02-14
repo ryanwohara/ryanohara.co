66815865a4ec07bd1fe3f80d5f2dd0a7b2754c1b
Ryan <ryan@ryanohara.co>
2015-02-14 01:26:33 -0500

    Initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		<link href='/css/app.css' rel='stylesheet' type='text/css'>
+
+		<script src="/js/script.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="content">
+				<div class="row">
+					<div class="title">Ryon</div>
+					<div class="quote">Code code code no matter what </div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
