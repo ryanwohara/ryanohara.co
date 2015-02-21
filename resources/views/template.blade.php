e8b1b5db487d04c37dbb6e17a93e28c87d4d9124
Vagrant User <vagrant@packer-virtualbox-iso.vagrantup.com>
2015-02-21 21:39:06 +0000

    Font Awesome\!

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -12,11 +12,21 @@
 					<div class="padding">
 						@yield('content')
 					</div>
+				</div>
+			</div>
+			<div class="row padding">
+				<div class="col-md-6 col-md-offset-3">
 					<a class="glyphicon glyphicon-home glyphicons-lg col-sm-offset-2 col-sm-2" title="Home" href="/"></a>
 					<a class="glyphicon glyphicon-user glyphicons-lg col-sm-2" aria-hidden="true" title="Resume" href="/about"></a>
 					<a class="glyphicon glyphicon-pencil glyphicons-lg col-sm-2" aria-hidden="true" title="Blog" href="/blog"></a>
 					<a class="glyphicon glyphicon-envelope glyphicons-lg col-sm-2" aria-hidden="true" title="Contact" id="contact" href="mailto:ryan@"></a>
-					<a id="twitter" class="twitter" href="https://twitter.com/sirrohara"></a>
+				</div>
+			</div>
+			<div class="row padding">
+                                <div class="col-md-6 col-md-offset-3">
+					<a id="twitter" class="fa fa-2x fa-twitter col-sm-offset-3 col-sm-2" href="https://twitter.com/sirrohara" target="_blank"></a>
+					<a id="linkedin" class="fa fa-2x fa-linkedin col-sm-2" href="https://www.linkedin.com/pub/ryan-w-o-hara/4b/ab9/824" target="_blank"></a>
+					<a id="github" class="fa fa-2x fa-github col-sm-2" href="https://github.com/ryanwohara" target="_blank"></a>
 				</div>
 			</div>
 		</div>
